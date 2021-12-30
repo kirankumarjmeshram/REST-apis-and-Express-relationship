58c2b403a15524de5a8e828be553018511971b83
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-12-31 05:29:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+const connect =()=>{
+    return mongoose.connect("mongodb://127.0.0.1:27017/kiran")
+};
+
 app.listen(1234,()=>{
     console.log("Server Listening on 1234")
 });
