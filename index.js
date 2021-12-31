361c87ecd8dc735c4d86144655e4e1e3cab7b35a
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-12-31 05:40:23 +0530

    User Schema and Model

--- index.js
+++ index.js
@@ -6,6 +6,32 @@ const connect =()=>{
     return mongoose.connect("mongodb://127.0.0.1:27017/kiran")
 };
 
+
+//USER Schema and MODEL
+const userSchema = new mongoose.Schema(
+    {
+        first_name : {type: String, required:true},
+        last_name : {type:String,required:false},
+        email: {type:String,required:true},
+        gender:{type: String, required:false,default:"Male"},
+    },
+    {
+        versionKey: false,
+        timeseries: true,
+    }
+);
+
+const User = mongoose.model("user",userSchema);
+
+
+
+
+
+
+
+
+
+
 app.listen(1234,()=>{
     console.log("Server Listening on 1234")
 });
